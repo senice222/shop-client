b33527b2915003592190643619236a6fc6d2014c
senice222 <artemprimorksij7@gmail.com>
2024-05-03 12:44:32 +0300

    updt

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const url = 'http://localhost:4000/internal'
-// const url = 'https://happyshop23.co/internal'
+const url = 'https://happyshop23.co/internal'
 
 const instance = axios.create({
     baseURL: url,
