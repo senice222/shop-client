6f773c858c2e836ba51bcf241027d847df53bc92
senice222 <artemprimorksij7@gmail.com>
2024-05-03 12:46:50 +0300

    updt

--- src/components/Counter/CounterDownTimer.jsx
+++ src/components/Counter/CounterDownTimer.jsx
@@ -17,7 +17,7 @@ const CountdownTimer = () => {
     const formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
     return (
-        <span>{formattedTime}</span>
+        <p>{formattedTime}</p>
     );
 };
 
