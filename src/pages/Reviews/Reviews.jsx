f17069c4dd554a6221ccf8fc8ecbde7da85443ab
senice222 <artemprimorksij7@gmail.com>
2024-05-04 21:36:31 +0300

    updt

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -4,10 +4,11 @@ import ReviewItem from '../../components/ReviewItem/ReviewItem'
 import instance from "../../core/axios";
 import {CreateCommentModal} from "../../components/Modals/CreateCommentModal/CreateCommentModal";
 import {useSelector} from "react-redux";
+
 const Reviews = () => {
     const [reviews, setReviews] = useState()
-    const user = useSelector((state) => state.user).data
     const [active, setActive] = useState()
+    const user = useSelector((state) => state.user).data
 
     const getReviews = async () => {
         try {
@@ -22,19 +23,21 @@ const Reviews = () => {
 
     useEffect(() => {
         getReviews()
-    }, [ ])
-  return (
-   <>
-       {user ? <CreateCommentModal update={getReviews} user={user} isAdmin={false} isOpened={active} setIsOpened={() => setActive(!active)}/> : null}
-       <div className={s.reviews}>
-           <h1>Отзывы</h1>
+    }, [])
+    return (
+        <>
+            {user ? <CreateCommentModal update={getReviews} user={user} isAdmin={false} isOpened={active}
+                                        setIsOpened={() => setActive(!active)}/> : null}
+            <div className={s.reviews}>
+                <h1>Отзывы</h1>
 
-           {reviews ? <div className={s.list}>
-               {reviews.map((item) => <ReviewItem date={item.date} name={item.productId} text={item.text} isAdmin={false} />)}
-           </div> : <h3>Loading...</h3>}
-       </div>
-   </>
-  )
+                {reviews ? <div className={s.list}>
+                    {reviews.map((item) => <ReviewItem date={item.date} name={item.productId} text={item.text}
+                                                       isAdmin={false}/>)}
+                </div> : <h3>Loading...</h3>}
+            </div>
+        </>
+    )
 }
 
 export default Reviews
